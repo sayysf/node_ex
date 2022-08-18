bb0e8e52d7f64ed21b53f5a9c90342dd5e71ef35
sayysf <sayysf@gmail.com>
2022-08-18 15:29:20 +0300

    save

--- index.ts
+++ index.ts
@@ -1,22 +1,58 @@
 const express = require('express');
 const app = express();
+
 const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const httpServer = http.createServer(app);
+
+const newLocal = "socket.io";
+const { Server } = require(newLocal);
+
+const io = new Server(httpServer);
+
+
+let con : any[];
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+/*
 io.on('connection', (socket) => {
     socket.on('chat message', (msg) => {
       console.log('message: ' + msg);
     });
+    
   });
+  */
 
-  
 
-server.listen(3000, () => {
+  io.connectio
+
+httpServer.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+async  function test(){
+
+  let i :number = 0;
+  let c  :any;
+
+  while(1)
+  {
+    con = io.on('connection');
+    if (con)
+    {
+      con.push(c);
+      c = 0;
+    }
+    con[i].on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+    if (con.length <= i)
+      i =0;
+    i++;
+  }
+
+}
+
+test();
